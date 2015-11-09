0616bc62f8a7bc284332daca3897f9ad8ed1c3e9
tommy <tooto1985@gmail.com>
2015-11-09 17:51:23 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 $(function() {
-	$(window).scroll(function() {
-
-		if (!$(".tip>img").hasClass("show") && $(window).scrollTop() > $(".tip").offset().top - parseInt($(".tip").css("margin-top"))  - $(window).height() + ($(".tip").outerHeight(true)/2)) {
-			
-				$(".tip>img").addClass("show");
-			
-		}
-
-	}).scroll();
+    $(window).load(function() {
+        $(window).scroll(function() {
+            if (!$(".tip>img").hasClass("show") && $(window).scrollTop() > $(".tip").offset().top - parseInt($(".tip").css("margin-top")) - $(window).height() + ($(".tip").outerHeight(true) / 2)) {
+                $(".tip>img").addClass("show");
+            }
+        }).scroll();
+    });
 });
